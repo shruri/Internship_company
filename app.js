931b8e4464e1cad844a163eeb585827ac2dc37cd
Shruti <87926659+shruri@users.noreply.github.com>
2023-06-11 18:00:40 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector(".bars");
+const nav = document.querySelector(".nav");
+menu.addEventListener("click", function(){
+    nav.classList.toggle("active");
+})
---
+++ send_email.php
@@ -0,0 +1,8 @@
+<?php
+   
+   if(isset($_POST['btn-send']))
+   {
+    echo 'working';
+   }
+
+?>
